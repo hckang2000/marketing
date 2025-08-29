16b9b38b40c072b835bc3444ce1b610477e5d612
hckang2000 <hckang2000@naver.com>
2025-08-29 20:42:07 +0900

    팀소개수정

--- src/components/sections/TeamIntroduction.tsx
+++ src/components/sections/TeamIntroduction.tsx
@@ -68,7 +68,7 @@ export function TeamIntroduction() {
                 자문의 강OO
               </h3>
               <p className="text-gray-600">
-                서울대학교 의학전문대학원 12 / 서울대병원 수련
+                서울대학교 의학전문대학원 14 / 서울대병원 수련
               </p>
             </motion.div>
           </div>
